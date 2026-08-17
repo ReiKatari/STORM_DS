7c1a483580deb06762bf9dae16b20efc075c74ae
ReiKatari <45316432+ReiKatari@users.noreply.github.com>
2026-08-17 15:53:37 +0300

    Release STORM DS v1.0.0: Voice TTS dialogue reading, 1-click Nintendo WFC online server switcher, Foldable tabletop mode, and version 1.0.0 milestone

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -4,6 +4,6 @@ object AppConfig {
     const val minSdkVersion = 24
     const val ndkVersion = "28.0.13004108"
 
-    const val versionCode = 44
-    const val versionName = "0.0.44"
+    const val versionCode = 100
+    const val versionName = "1.0.0"
 }
